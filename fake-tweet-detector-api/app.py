1616286b4e2dd470ef180afd5e15fc99208eb288
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-12 22:35:49 +0000

    Fix entry point

--- fake-tweet-detector-api/app.py
+++ fake-tweet-detector-api/app.py
@@ -6,29 +6,9 @@ import os
 
 app = Flask(__name__)
 
-# Load the trained model and vectorizer
-model = joblib.load('knn_model.pkl') 
-vectorizer = joblib.load('vectorizer.pkl') 
-
-@app.route('/classify', methods=['POST'])
-def classify_tweet():
-    # Get tweet data from the incoming request
-    data = request.json
-    tweet = data.get('tweet', '')
-    
-    if not tweet:
-        return jsonify({'error': 'No tweet provided'}), 400
-    
-    # Preprocess the tweet using the same method as during training
-    processed_tweet = vectorizer.transform([tweet])  # Convert tweet to feature vector
-    
-    # Predict using the model
-    prediction = model.predict(processed_tweet)
-    
-    # Classify as "Fake" or "Real"
-    result = "Fake" if prediction[0] == 1 else "Real"
-    
-    return jsonify({'prediction': result})
+@app.route('/')
+def home():
+    return "Hello, Heroku!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Get the port from environment or default to 5000
