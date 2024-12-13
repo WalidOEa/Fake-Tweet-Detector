0d2ac5cb6ec618cb2d13ebf49dbb955a3633d8b0
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-13 19:04:38 +0000

    Update app.py

--- fake-tweet-detector-api/app.py
+++ fake-tweet-detector-api/app.py
@@ -1,35 +1,30 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import numpy as np
-
 import os
 
 app = Flask(__name__)
+CORS(app)  # This will allow all origins to make requests to your API
 
 # Load the trained model and vectorizer
-model = joblib.load('fake-tweet-detector-api/knn_model.pkl') 
-vectorizer = joblib.load('fake-tweet-detector-api/vectorizer.pkl') 
+model = joblib.load('knn_model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
 
 @app.route('/classify', methods=['POST'])
 def classify_tweet():
-    # Get tweet data from the incoming request
-    data = request.json
+    data = request.get_json()
     tweet = data.get('tweet', '')
     
     if not tweet:
         return jsonify({'error': 'No tweet provided'}), 400
     
-    # Preprocess the tweet using the same method as during training
-    processed_tweet = vectorizer.transform([tweet])  # Convert tweet to feature vector
-    
-    # Predict using the model
+    processed_tweet = vectorizer.transform([tweet])
     prediction = model.predict(processed_tweet)
-    
-    # Classify as "Fake" or "Real"
     result = "Fake" if prediction[0] == 1 else "Real"
     
     return jsonify({'prediction': result})
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 5000))  # Default to 5000 if $PORT is not set
-   app.run(debug=False, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
