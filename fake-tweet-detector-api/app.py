a0031145d7c4e7cbe97e85dd79ea0b69d7af0111
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-13 18:03:33 +0000

    Initial commit for Heroku deployment

--- fake-tweet-detector-api/app.py
+++ fake-tweet-detector-api/app.py
@@ -11,5 +11,4 @@ def home():
     return "Hello, Heroku!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get the port from environment or default to 5000
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0')
