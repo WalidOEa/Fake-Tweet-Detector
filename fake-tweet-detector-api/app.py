1f0e4d7a36ff34fce00078d1f6524f6c2c0c4e5c
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-13 18:26:50 +0000

    Bind app to  for Heroku

--- fake-tweet-detector-api/app.py
+++ fake-tweet-detector-api/app.py
@@ -11,4 +11,5 @@ def home():
     return "Hello, Heroku!"
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+   port = int(os.environ.get("PORT", 5000))  # Default to 5000 if $PORT is not set
+   app.run(debug=False, host='0.0.0.0', port=port)
